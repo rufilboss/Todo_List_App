92f58e97d19583c3f9077de8994c48405125e21c
rufilboy <rufaiilyas600@gmail.com>
2022-04-19 09:48:05 +0100

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<h1 align="center">
- A To DO List Web APP
-</h1>
+# Django-To-Do-list-with-user-authentication
+To Do list app with User Registration, Login, Search and full Create Read Update and DELETE functionality.
 
-## Coming Soon...
+<!-- ![DEMO](../master/Django%20To%20Do%20List%20App.jpg) -->
+
--- base/models.py
+++ base/models.py
@@ -1,11 +1,11 @@
-from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
-
 # Create your models here.
 
+
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
@@ -15,4 +15,4 @@ class Task(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['complete']
+        order_with_respect_to = 'user'
